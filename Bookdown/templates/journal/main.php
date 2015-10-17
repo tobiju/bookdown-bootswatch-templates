26280a32f66685dfcc1ead8c1b1155a17829e0a5
tobiju <t.jueschke@googlemail.com>
2015-10-17 13:52:51 +0200

    Added bootswatch styles to bookdown templates

---
+++ Bookdown/templates/journal/main.php
@@ -0,0 +1,24 @@
+<?php
+// default library templates
+$library = dirname(dirname(__DIR__)) . "/vendor/bookdown/bookdown/templates/";
+
+// project-specific templates
+$template_path = __DIR__;
+
+// register the templates
+$templates = $this->getViewRegistry();
+
+$templates->set("head", "{$template_path}/head.php");
+$templates->set("body", "{$library}/body.php");
+$templates->set("core", "{$library}/core.php");
+$templates->set("navheader", "{$template_path}/../navheader.php");
+$templates->set("navfooter", "{$template_path}/../navfooter.php");
+$templates->set("toc", "{$template_path}/../toc.php");
+?>
+
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8" />
+    <?php echo $this->render("head"); ?>
+    <?php echo $this->render("body"); ?>
+</html>
