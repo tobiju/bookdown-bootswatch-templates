9ebec60473b70013e9f4e06348673c23b49d1ab1
Sandro Keil <github@sandro-keil.de>
2015-11-20 19:24:50 +0100

    added contrib files, updated readme, changed default styles for lighter colors, pinned bookdown to 0.5.0

---
+++ CONTRIBUTING.md
@@ -0,0 +1,12 @@
+# CONTRIBUTING
+
+## RESOURCES
+
+If you wish to contribute to bookdown-bootswatch-templates, please be sure to read to the following resources:
+
+ -  Coding Standards: [PSR-0/1/2/4](https://github.com/php-fig/fig-standards/tree/master/accepted)
+ -  Git Guide: [README-GIT.md](README-GIT.md)
+
+If you are working on new features, or refactoring an existing
+component, please create a proposal. You can do this in on the RFC's
+page, https://github.com/tobiju/bookdown-bootswatch-templates/wiki/RFCs.
--- templates/toc.php
+++ templates/toc.php
@@ -1,6 +1,16 @@
-<?php if (!$this->page->hasTocEntries()) {
-    return;
-} ?>
+<?php
+/**
+ * tobiju
+ *
+ * @link      https://github.com/tobiju/bookdown-bootswatch-templates for the canonical source repository
+ * @copyright Copyright (c) 2015 Tobias Jüschke
+ * @license   https://github.com/tobiju/bookdown-bootswatch-templates/blob/master/LICENSE.txt New BSD License
+ */
+
+    if (!$this->page->hasTocEntries()) {
+        return;
+    }
+?>
 
 <h1><?= $this->page->getNumberAndTitle(); ?></h1>
 <table class="table table-striped">
